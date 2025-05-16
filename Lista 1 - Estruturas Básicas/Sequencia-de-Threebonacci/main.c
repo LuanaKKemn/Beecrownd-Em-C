68fe99603ce96865865438f8bc707c3d3e965a50
LuanaKKemn <luanakkemndtooa@gmail.com>
2025-05-15 22:52:25 -0300

    Estruturas Básicas

---
+++ Lista 1 - Estruturas Básicas/Sequencia-de-Threebonacci/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int fibonacci(int f){
+  int fibo[f] = {};
+  fibo[0] = 1;
+  fibo[1] = 1;
+  for (int i = 2; i < f; i++)
+    fibo[i] = fibo[i-1]+fibo[-2];
+  return fibo[f];
+}
+
+int isMultiplo(int valor){
+  if (valor % 3 == 0)
+    return 1;
+}
+
+int isTres(int numero){
+  int nt, cc;
+  cc = 0;
+  int = n;
+  
+  int w;
+  w = 0;
+  while (w == 0)
+  if (nt / 10 ==0){
+    
+  }
+  eslse{
+    nt = nt / 10;
+    cc++
+    
+  
+      
+  w = 0;
+  while (w == 0){
+    
+  }
+  
+  
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  
+  
+  return 0;
+}
